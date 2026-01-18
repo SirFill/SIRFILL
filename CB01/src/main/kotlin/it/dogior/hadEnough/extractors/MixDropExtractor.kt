302eceb7971a372191d1e173ea03a2dad65fe35e
DieGon7771 <bruttodiego6@gmail.com>
2026-01-18 21:31:38 +0100

    Update MixDropExtractor.kt

--- CB01/src/main/kotlin/it/dogior/hadEnough/extractors/MixDropExtractor.kt
+++ CB01/src/main/kotlin/it/dogior/hadEnough/extractors/MixDropExtractor.kt
@@ -3,5 +3,5 @@ package it.dogior.hadEnough.extractors
 import com.lagradost.cloudstream3.extractors.MixDrop
 
 class MixDropExtractor : MixDrop(){
-    override var mainUrl = "https://mixdrop.ag"
+    override var mainUrl = "https://m1xdrop.net"
 }
