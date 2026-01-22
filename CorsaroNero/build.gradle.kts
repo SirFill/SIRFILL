0b0ed5e6c8e3889d13374fdcb46264a6e19edc53
DieGon7771 <bruttodiego6@gmail.com>
2026-01-22 21:17:52 +0100

    Update build.gradle.kts

--- CorsaroNero/build.gradle.kts
+++ CorsaroNero/build.gradle.kts
@@ -1,7 +1,7 @@
 import org.jetbrains.kotlin.konan.properties.Properties
 
 // use an integer for version numbers
-version = 4
+version = 5
 
 
 cloudstream {
@@ -17,7 +17,7 @@ cloudstream {
      * 2: Slow
      * 3: Beta only
      * */
-    status = 0
+    status = 2
 
     tvTypes = listOf("Movie", "Torrent")
 
