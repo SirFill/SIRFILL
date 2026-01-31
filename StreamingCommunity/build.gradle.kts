39cccfa9051adb17b498b1c95ec9ca77c1962f58
SirFill <alexmarry02@gmail.com>
2026-01-31 14:42:17 +0100

    Update authors and iconUrl in build.gradle.kts

--- StreamingCommunity/build.gradle.kts
+++ StreamingCommunity/build.gradle.kts
@@ -6,7 +6,7 @@ cloudstream {
     // All of these properties are optional, you can safely remove them
 
     description = "Film e SerieTV da StreamingCommunity"
-    authors = listOf("doGior","DieGon")
+    authors = listOf("doGior","DieGon","SirFill)
 
     /**
     * Status int as the following:
@@ -26,7 +26,7 @@ cloudstream {
     requiresResources = true
     language = "it"
 
-    iconUrl = "https://raw.githubusercontent.com/DieGon7771/ItaliaInStreaming/master/StreamingCommunity/streamingunity_icon.png"
+    iconUrl = "https://raw.githubusercontent.com/SirFill/SIRFILL/master/StreamingCommunity/streamingunity_icon.png"
 }
 
 android {
